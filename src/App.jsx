9c265b6fce1d8d486530c4feff51f0eb4702fb57
Mustafa Binalhag <mbin828@gmail.com>
2022-11-01 10:58:57 -0400

    react demo

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Hello from './Hello.jsx'
+function App() {
+
+  return (
+    <div className="App">
+      <Hello name="Mustafa"/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Hello.jsx
@@ -0,0 +1,8 @@
+const Hello = (props) => {
+  return (
+    <>
+      <h1> HELLO my name is {props.name} </h1>
+    </>
+  );
+}
+export default Hello;
